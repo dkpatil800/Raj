76e3b9bf9fb302ac8e3c3742bcf99ca56f6b9055
dkpatil <131771207+dkpatil800@users.noreply.github.com>
2024-11-27 21:47:27 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,39 @@
+ 
+ <!-- Footer  -->
+ <div class="container-fluid bg-white mt-5">
+            <div class="row">
+              <div class="col-lg-4 p-4">
+                <h3 class="h-font fw-bold fs-3 mb-2">R<span style="color:red;">@</span>J Hotel</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur tempora veritatis, voluptatibus, rem magnam veniam tenetur debitis iste ipsam rerum, maxime molestias nam dolor beatae reiciendis excepturi hic officia minima.
+                </p>
+              </div>
+              <div class="col-lg-4 p-4">
+                <h5 class="mb-3">Links</h5>
+                <a href="index.php" class="d-inline-block mb-2 text-dark text-decoration-none">Home </a><br>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms </a><br>
+                <a href="facilities.php" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities </a><br>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact us </a><br>
+                <a href="about.php" class="d-inline-block mb-2 text-dark text-decoration-none">About </a><br>
+              </div>
+              <div class="col-lg-4 p-4">
+                <h5 class="mb-3">Follow us</h5>
+                <a href="#" class="d-inline-block mb-2  text-dark text-decoration-none">
+                  <i class="bi bi-twitter me-1"></i>Twitter 
+                </a>
+                <br>
+                <a href="#" class="d-inline-block mb-2  text-dark text-decoration-none">
+                  <i class="bi bi-facebook me-1"></i>Facebook 
+                </a>
+                <br>
+                <a href="#" class="d-inline-block text-dark text-decoration-none">
+                  <i class="bi bi-instagram me-1"></i>Instagram 
+                </a>
+                <br>
+              </div>
+            </div>
+          </div>
+      <h6 class="bg-dark text-center text-white p-3 m-0">Design and Developed by Mohit Ingale <i class="bi bi-telephone-outbound p-2"></i><a href="tel: +919370940227" class="text-blue text-decoration-none">Contact developer</a></h6>
+          
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
